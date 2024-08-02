5dc964188278822ad99748d85e002c9b9799432c
mueiya <mueiya.whatever@gmail.com>
2024-08-03 00:27:31 +0700

    refactor: Update AlbumRepositoryPostgres class
    This commit refactors the `AlbumRepositoryPostgres` class in the `src/infrastructures/repository` directory. The changes include improving code formatting and removing unnecessary comments. The functionality of the class remains unchanged.
    
    Refactoring the code improves readability and maintainability.

--- src/infrastructures/repository/AlbumRepositoryPostgres.js
+++ src/infrastructures/repository/AlbumRepositoryPostgres.js
@@ -2,36 +2,58 @@ const NotFoundError = require("../../commons/exceptions/NotFoundError");
 const AlbumRepository = require("../../domains/albums/AlbumRepository");
 
 class AlbumRepositoryPostgres extends AlbumRepository {
-    constructor (pool, idGenerator) {
-        super();
-        this._pool = pool;
-        this._idGenerator = idGenerator;
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async postAlbum(postAlbumEntity) {
+    const { name, year } = postAlbumEntity;
+    const id = `album-${this._idGenerator()}`;
+    const query = {
+      text: "INSERT INTO albums VALUES($1, $2, $3) RETURNING id",
+      values: [id, name, year],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows[0];
+  }
+
+  async getAlbumById(albumId) {
+    const query = {
+      text: "SELECT * FROM albums WHERE id = $1",
+      values: [albumId],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError("album tidak ditemukan");
     }
+    return result.rows[0];
+  }
 
-    async postAlbum(postAlbumEntity) {
-        const { name, year } = postAlbumEntity;
-        const id = `album-${this._idGenerator()}`;
-        const query = {
-            text: 'INSERT INTO albums VALUES($1, $2, $3) RETURNING id',
-            values: [id, name , year],
-        };
+  async verifyAlbumById(albumId) {
+    const query = {
+      text: "SELECT id FROM albums WHERE id = $1",
+      values: [albumId],
+    };
 
-        const result = await this._pool.query(query);
-        return result.rows[0];
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError("album tidak ditemukan");
     }
+  }
 
-    async getAlbumById(albumId) {
-        const query = {
-            text: 'SELECT * FROM albums WHERE id = $1',
-            values: [albumId],
-        };
+  async putAlbumById(albumId, putAlbumEntity) {
+    const { name, year } = putAlbumEntity;
+    const query = {
+      text: "UPDATE albums SET name = $1, year = $2 WHERE id = $3 RETURNING id",
+      values: [name, year, albumId],
+    };
 
-        const result = await this._pool.query(query);
-        if (!result.rowCount) {
-            throw new NotFoundError('album tidak ditemukan');
-        }
-        return result.rows[0];
-    }
+    await this._pool.query(query);
+  }
 }
 
 module.exports = AlbumRepositoryPostgres;
