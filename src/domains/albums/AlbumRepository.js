3ad2120b62aded2d0bf2971184cf02ac07590017
mueiya <mueiya.whatever@gmail.com>
2024-08-03 00:34:24 +0700

    feat: Add deleteAlbumById method to AlbumRepository
    This commit adds the `deleteAlbumById` method to the `AlbumRepository` class in the `src/domains/albums/AlbumRepository.js` file. The method is responsible for deleting an album by its ID. It throws an error with the message "ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED" to indicate that the method is not yet implemented.
    
    This code change provides the foundation for implementing the functionality to delete an album in the application.

--- src/domains/albums/AlbumRepository.js
+++ src/domains/albums/AlbumRepository.js
@@ -11,6 +11,9 @@ class AlbumRepository {
     async putAlbumById(albumId, putAlbumEntity) {
         throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
     }
+    async deleteAlbumById(albumId) {
+        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    }
 }
 
 module.exports = AlbumRepository;
