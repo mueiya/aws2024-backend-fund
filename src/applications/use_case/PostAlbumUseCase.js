809e0ce1d720fad8db44ea6f89aeaef7d3f5baf1
mueiya <mueiya.whatever@gmail.com>
2024-08-03 00:12:59 +0700

    feat: Implement PostAlbumUseCase
    This commit adds the `PostAlbumUseCase` class to the `applications/use_case` directory. The `PostAlbumUseCase` class is responsible for executing the use case of posting an album. It takes a payload object, creates a `PostAlbum` entity, and calls the `postAlbum` method of the `albumRepository` to store the album data. The result is then transformed into a `PostedAlbum` entity and returned.
    
    This use case provides the functionality to create and store albums in the application.

---
+++ src/applications/use_case/PostAlbumUseCase.js
@@ -0,0 +1,16 @@
+const PostAlbum = require("../../domains/albums/entities/PostAlbum");
+const PostedAlbum = require("../../domains/albums/entities/PostedAlbum");
+
+class PostAlbumUseCase {
+  constructor({ albumRepository }) {
+    this._albumRepository = albumRepository;
+  }
+
+  async execute(useCasePayload) {
+    const postAlbumEntity = new PostAlbum(useCasePayload);
+    const postAlbumResult = await this._albumRepository.postAlbum(postAlbumEntity);
+    return new PostedAlbum(postAlbumResult);
+  }
+}
+
+module.exports = PostAlbumUseCase;
