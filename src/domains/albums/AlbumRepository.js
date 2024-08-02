68c2e5c13b7793468a01758e4b426bb144fd5ea6
mueiya <mueiya.whatever@gmail.com>
2024-08-03 00:27:16 +0700

    feat: Add methods for verifying and updating album data

--- src/domains/albums/AlbumRepository.js
+++ src/domains/albums/AlbumRepository.js
@@ -5,6 +5,12 @@ class AlbumRepository {
     async getAlbumById(albumId) {
         throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
     }
+    async verifyAlbumById(albumId) {
+        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    }
+    async putAlbumById(albumId, putAlbumEntity) {
+        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    }
 }
 
 module.exports = AlbumRepository;
