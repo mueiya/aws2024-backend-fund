32bfdc73e85197d5e3b99a0b740d665856854bd8
mueiya <mueiya.whatever@gmail.com>
2024-08-03 00:12:32 +0700

    feat: Add PostAlbum and PostedAlbum entities
    This commit adds two new entity classes, `PostAlbum` and `PostedAlbum`, to the `albums` domain. These classes handle the creation and validation of album data.
    
    The `PostAlbum` class takes a payload object with `name` and `year` properties and validates the payload before assigning the values to the corresponding properties. If the payload is invalid, an error is thrown.
    
    The `PostedAlbum` class takes a payload object with an `id` property and validates the payload before assigning the value to the `id` property. If the payload is invalid, an error is thrown.
    
    These new entities provide a foundation for handling album data in the application.

---
+++ src/domains/albums/entities/PostAlbum.js
@@ -0,0 +1,20 @@
+class PostAlbum {
+  constructor(payload) {
+    this._validatePayload(payload);
+
+    this.name = payload.name;
+    this.year = payload.year;
+  }
+
+  _validatePayload({ name, year }) {
+    if (!name || !year) {
+      throw new Error("POST_ALBUM.NOT_CONTAIN_NEEDED_PROPERTY");
+    }
+
+    if (typeof name !== "string" || typeof year !== "number") {
+      throw new Error("POST_ALBUM.NOT_MEET_DATA_TYPE_SPECIFICATION");
+    }
+  }
+}
+
+module.exports = PostAlbum;
---
+++ src/domains/albums/entities/PostedAlbum.js
@@ -0,0 +1,19 @@
+class PostedAlbum {
+  constructor(payload) {
+    this._validatePayload(payload);
+
+    this.id = payload.id;
+  }
+
+  _validatePayload({ id }) {
+    if (!id) {
+      throw new Error("POSTED_ALBUM.NOT_CONTAIN_NEEDED_PROPERTY");
+    }
+
+    if (typeof id !== "string") {
+      throw new Error("POSTED_ALBUM.NOT_MEET_DATA_TYPE_SPECIFICATION");
+    }
+  }
+}
+
+module.exports = PostedAlbum;
