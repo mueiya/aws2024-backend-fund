c0c6789c63d9367f09af6b07eaaa163db1accb4b
mueiya <mueiya.whatever@gmail.com>
2024-08-03 00:12:49 +0700

    feat: Add AlbumRepository class
    This commit adds the `AlbumRepository` class to the `albums` domain. This class provides methods for posting and retrieving album data. The `postAlbum` method is used to create a new album by accepting a `postAlbumEntity` object and throwing an error if the method is not implemented. The `getAlbumById` method is used to retrieve an album by its ID, also throwing an error if the method is not implemented. This class serves as the repository for handling album data in the application.

---
+++ src/domains/albums/AlbumRepository.js
@@ -0,0 +1,10 @@
+class AlbumRepository {
+    async postAlbum(postAlbumEntity) {
+        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    }
+    async getAlbumById(albumId) {
+        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    }
+}
+
+module.exports = AlbumRepository;
