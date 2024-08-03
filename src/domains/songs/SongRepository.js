91f33ffbcfaec882c2eb5d812cbae966d559db95
mueiya <mueiya.whatever@gmail.com>
2024-08-03 21:25:10 +0700

    feat: Implement getSongs method in SongRepository
    This commit adds the `getSongs` method to the `SongRepository` class in the `src/domains/songs/SongRepository.js` file. The method is responsible for retrieving songs from the song repository. This functionality enables the application to fetch songs and use them for further processing.
    
    These code changes enable the application to retrieve songs using the `getSongs` method in the `SongRepository` class.

--- src/domains/songs/SongRepository.js
+++ src/domains/songs/SongRepository.js
@@ -2,6 +2,9 @@ class SongRepository {
     async postSong(postSongEntity) {
         throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
     }
+    async getSongs() {
+        throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    }
 }
 
 module.exports = SongRepository;
