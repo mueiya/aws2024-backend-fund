1a9bc1854c41addda56d6534f2bb8174bda99d9d
mueiya <mueiya.whatever@gmail.com>
2024-08-03 21:14:05 +0700

    feat: Add SongsHandler and SongsRepository classes for handling song requests and database operations
    These code changes add the `SongsHandler` class to handle song requests and the `SongsRepository` class to perform database operations related to songs. The `SongsHandler` class provides the necessary routes and methods for handling song requests, while the `SongsRepository` class implements the logic for interacting with the database to retrieve and store song data.
    
    This commit enables the application to handle song-related functionality and ensures proper separation of concerns between the handler and repository layers.

--- src/infrastructures/di-container.js
+++ src/infrastructures/di-container.js
@@ -1,11 +1,18 @@
 const { nanoid } = require("nanoid");
 const pool = require("./database/postgres/pool");
+//repositories
 const AlbumRepositoryPostgres = require("./repository/AlbumRepositoryPostgres");
+const SongRepositoryPostgres = require("./repository/SongRepositoryPostgres");
+// album use cases
 const PostAlbumUseCase = require("../applications/use_case/PostAlbumUseCase");
-const AlbumsHandler = require("../interfaces/http/api/albums/handlers");
 const GetAlbumByIdUseCase = require("../applications/use_case/GetAlbumByIdUseCase");
 const PutAlbumByIdUseCase = require("../applications/use_case/PutAlbumByIdUseCase");
 const DeleteAlbumByIdUseCase = require("../applications/use_case/DeleteAlbumByIdUseCase");
+// song use case
+const PostSongUseCase = require("../applications/use_case/PostSongUseCase");
+// handlers
+const AlbumsHandler = require("../interfaces/http/api/albums/handlers");
+const SongsHandler = require("../interfaces/http/api/songs/handlers");
 
 /**
  * Albums Dependency Injection Container
@@ -25,14 +32,31 @@ const putAlbumByIdUseCase = new PutAlbumByIdUseCase({
 const deleteAlbumByIdUseCase = new DeleteAlbumByIdUseCase({
   albumRepository: albumRepositoryPostgres,
 });
-// Handler
+
+/**
+ * Songs Dependency Injection Container
+ */
+// Repository
+const songRepositoryPostgres = new SongRepositoryPostgres(pool, nanoid);
+// Use Case
+const postSongUseCase = new PostSongUseCase({
+  songRepository: songRepositoryPostgres,
+});
+
+/**
+ * Instantiate Handlers
+ */
 const albumsHandler = new AlbumsHandler({
   postAlbumUseCase,
   getAlbumByIdUseCase,
   putAlbumByIdUseCase,
   deleteAlbumByIdUseCase,
 });
+const songsHandler = new SongsHandler({
+  postSongUseCase,
+})
 
 module.exports = {
   albumsHandler,
+  songsHandler,
 };
