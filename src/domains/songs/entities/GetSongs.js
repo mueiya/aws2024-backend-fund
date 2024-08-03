adb4cad509a1e34bceb6a4aa32307a82e8e39120
mueiya <mueiya.whatever@gmail.com>
2024-08-03 21:24:57 +0700

    feat: Add GetSongs entity for retrieving song data
    This commit adds the `GetSongs` entity to the `src/domains/songs/entities/GetSongs.js` file. The entity represents the data structure for retrieving song data and includes properties for `id`, `title`, and `performer`. The entity also includes a validation method `_validatePayload` to ensure that the required properties are present and have the correct data types.
    
    These code changes enable the application to retrieve song data using the `GetSongs` entity.

---
+++ src/domains/songs/entities/GetSongs.js
@@ -0,0 +1,21 @@
+class GetSongs {
+  constructor(payload) {
+    this._validatePayload(payload);
+
+    this.id = payload.id;
+    this.title = payload.title;
+    this.performer = payload.performer;
+  }
+
+    _validatePayload({ id, title, performer }) {
+        if (!id || !title || !performer) {
+        throw new Error("GET_SONG.NOT_CONTAIN_NEEDED_PROPERTY");
+        }
+    
+        if (typeof id !== "string" || typeof title !== "string" || typeof performer !== "string") {
+        throw new Error("GET_SONG.NOT_MEET_DATA_TYPE_SPECIFICATION");
+        }
+    }
+}
+
+module.exports = GetSongs;
