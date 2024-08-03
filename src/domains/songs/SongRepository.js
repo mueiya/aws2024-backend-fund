ed891117e074c29a5c7e1f2b2b674e0415f705be
mueiya <mueiya.whatever@gmail.com>
2024-08-03 21:54:49 +0700

    feat: Add deleteSongById method to SongRepository
    This commit adds the `deleteSongById` method to the `SongRepository` class in the `src/domains/songs/SongRepository.js` file. The method is responsible for deleting a song from the song repository by its ID. This functionality enables the application to remove specific songs from the repository.

--- src/domains/songs/SongRepository.js
+++ src/domains/songs/SongRepository.js
@@ -1,19 +1,22 @@
 class SongRepository {
-    async postSong(postSongEntity) {
-        throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
-    async getSongs() {
-        throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
-    async getSongById(songId) {
-        throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
-    async verifySongById(songId) {
-        throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
-    async putSongById(songId, putSongEntity) {
-        throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
+  async postSong(postSongEntity) {
+    throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async getSongs() {
+    throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async getSongById(songId) {
+    throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async verifySongById(songId) {
+    throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async putSongById(songId, putSongEntity) {
+    throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async deleteSongById(songId) {
+    throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
 }
 
 module.exports = SongRepository;
