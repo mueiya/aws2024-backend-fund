5dc28a32128c6fe12f271ce9833297c908efded1
mueiya <mueiya.whatever@gmail.com>
2024-08-03 21:49:50 +0700

    refactor: Update error messages in DomainErrorTranslator.js

--- src/commons/exceptions/DomainErrorTranslator.js
+++ src/commons/exceptions/DomainErrorTranslator.js
@@ -45,6 +45,18 @@ DomainErrorTranslator._directories = {
   "POSTED_SONG.NOT_MEET_DATA_TYPE_SPECIFICATION": new InvariantError(
     "data type is not meet the specification"
   ),
+  "PUT_SONG.NOT_CONTAIN_NEEDED_PROPERTY": new InvariantError(
+    "property that needed is not provided"
+  ),
+  "PUT_SONG.NOT_MEET_DATA_TYPE_SPECIFICATION": new InvariantError(
+    "data type is not meet the specification"
+  ),
+  "GET_SONG.NOT_CONTAIN_NEEDED_PROPERTY": new InvariantError(
+    "property that needed is not provided"
+  ),
+  "GET_SONG.NOT_MEET_DATA_TYPE_SPECIFICATION": new InvariantError(
+    "data type is not meet the specification"
+  ),
 };
 
 module.exports = DomainErrorTranslator;
