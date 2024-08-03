a268508ce8cd8e6239b7dec792fabc659bb09042
mueiya <mueiya.whatever@gmail.com>
2024-08-03 21:25:16 +0700

    feat: Add getSongs method to SongRepositoryPostgres
    This commit adds the `getSongs` method to the `SongRepositoryPostgres` class in the `src/infrastructures/repository/SongRepositoryPostgres.js` file. The method retrieves songs from the song repository by executing a SELECT query on the `songs` table. This functionality enables the application to fetch songs from the PostgreSQL database.
    
    ```

--- src/infrastructures/repository/SongRepositoryPostgres.js
+++ src/infrastructures/repository/SongRepositoryPostgres.js
@@ -1,23 +1,31 @@
 const SongRepository = require("../../domains/songs/SongRepository");
 
 class SongRepositoryPostgres extends SongRepository {
-    constructor(pool, idGenerator) {
-        super();
-        this._pool = pool;
-        this._idGenerator = idGenerator
-    }
-    async postSong(postSongEntity) {
-        const { title, year, genre, performer, duration, albumId } = postSongEntity;
-        const id = `song-${this._idGenerator()}`;
-        console.log(id, title, year, genre, performer, duration, albumId);
-        const query = {
-            text: "INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id",
-            values: [id, title, year, genre, performer, duration, albumId]
-        };
-        const result = await this._pool.query(query);
-        console.log(result);
-        return result.rows[0];
-    }
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+  async postSong(postSongEntity) {
+    const { title, year, genre, performer, duration, albumId } = postSongEntity;
+    const id = `song-${this._idGenerator()}`;
+    console.log(id, title, year, genre, performer, duration, albumId);
+    const query = {
+      text: "INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+      values: [id, title, year, genre, performer, duration, albumId],
+    };
+    const result = await this._pool.query(query);
+    console.log(result);
+    return result.rows[0];
+  }
+
+  async getSongs() {
+    const query = {
+      text: "SELECT * FROM songs",
+    };
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
 }
 
 module.exports = SongRepositoryPostgres;
