1bccbb4301dd612e2a7023867c787fbb1d81de95
mueiya <mueiya.whatever@gmail.com>
2024-08-12 15:14:26 +0700

    refactor: exclude rule for unused vars parameter in repository domain

--- src/domains/albums/AlbumRepository.js
+++ src/domains/albums/AlbumRepository.js
@@ -1,19 +1,20 @@
+/* eslint-disable no-unused-vars */
 class AlbumRepository {
-    async postAlbum(postAlbumEntity) {
-        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
-    async getAlbumById(albumId) {
-        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
-    async verifyAlbumById(albumId) {
-        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
-    async putAlbumById(albumId, putAlbumEntity) {
-        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
-    async deleteAlbumById(albumId) {
-        throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
-    }
+  async postAlbum(postAlbumEntity) {
+    throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async getAlbumById(albumId) {
+    throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async verifyAlbumById(albumId) {
+    throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async putAlbumById(albumId, putAlbumEntity) {
+    throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+  async deleteAlbumById(albumId) {
+    throw new Error("ALBUM_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
 }
 
 module.exports = AlbumRepository;
--- src/domains/songs/SongRepository.js
+++ src/domains/songs/SongRepository.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 class SongRepository {
   async postSong(postSongEntity) {
     throw new Error("SONG_REPOSITORY.METHOD_NOT_IMPLEMENTED");
